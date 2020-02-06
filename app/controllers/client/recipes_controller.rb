19be5a1a73568917dbb8f9dcd817dc63cade06ea
tommy3wood <thomaswcurran1014@gmail.com>
2020-02-06 13:34:21 -0600

    REST adds new/create

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -4,4 +4,31 @@ class Client::RecipesController < ApplicationController
     @recipes = response.parse
     render 'index.html.erb'
   end
+
+  def new
+    render 'new.html.erb'
+  end
+
+  def create
+    client_params = {
+                      title: params[:title],
+                      prep_time: params[:prep_time],
+                      ingredients: params[:ingredients],
+                      directions: params[:directions],
+                      image_url: params[:image_url]
+                      }
+    response = HTTP.post(
+                          "http://localhost:3000/api/recipes", 
+                          form: client_params
+                          )
+    render 'create.html.erb'
+  end
+
+  def show
+    response = HTTP.get("http://localhost:3000/api/recipes/#{params[:id]}")
+    @recipe = response.parse
+    render 'show.html.erb'
+  end
+
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   namespace :client do
     get '/recipes' => 'recipes#index'
+    get '/recipes/new' => 'recipes#new'
+    post '/recipes' => 'recipes#create'
+    get '/recipes/:id' => 'recipes#show'
+
+
   end
 end
