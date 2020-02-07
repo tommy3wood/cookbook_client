7e06c4ee50f77bad6615a4621f4c56e9eaaf392c
tommy3wood <thomaswcurran1014@gmail.com>
2020-02-07 11:57:54 -0600

    edit/update/destroy

--- app/controllers/client/recipes_controller.rb
+++ app/controllers/client/recipes_controller.rb
@@ -21,7 +21,8 @@ class Client::RecipesController < ApplicationController
                           "http://localhost:3000/api/recipes", 
                           form: client_params
                           )
-    render 'create.html.erb'
+    recipe = response.parse
+    redirect_to "/client/recipes/#{ recipe["id"] }"
   end
 
   def show
@@ -30,5 +31,30 @@ class Client::RecipesController < ApplicationController
     render 'show.html.erb'
   end
 
+  def edit
+    response = HTTP.get("http://localhost:3000/api/recipes/#{ params[:id] }")
+    @recipe = response.parse
+    render 'edit.html.erb'
+  end
 
+  def update
+    client_params = {
+                      title: params[:title],
+                      prep_time: params[:prep_time],
+                      ingredients: params[:ingredients],
+                      directions: params[:directions],
+                      image_url: params[:image_url]
+                      }
+    response = HTTP.patch(
+                          "http://localhost:3000/api/recipes/#{params[:id]}", 
+                          form: client_params
+                          )
+    recipe = response.parse
+    redirect_to "/client/recipes/#{recipe["id"]}"
+  end
+
+  def destroy
+    response = HTTP.delete("http://localhost:3000/api/recipes/#{params[:id]}")
+    redirect_to '/client/recipes'
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
     get '/recipes/new' => 'recipes#new'
     post '/recipes' => 'recipes#create'
     get '/recipes/:id' => 'recipes#show'
-
-
+    get '/recipes/:id/edit' => 'recipes#edit'
+    patch '/recipes/:id' => 'recipes#update'
+    delete '/recipes/:id' => 'recipes#destroy'
   end
 end
